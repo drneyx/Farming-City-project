aaa1c874c89a541b3de108531d90594a61c8c509
Nathani Luving <cosmasnathan9@gmail.com>
2022-08-13 13:12:14 +0300

    Add project news

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\News;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('frontend.home.home');
+        $news = News::latest()->take(3)->get();
+        return view('frontend.home.home', compact('news'));
     }
 }
--- resources/views/frontend/template/list.blade.php
+++ resources/views/frontend/template/list.blade.php
@@ -48,14 +48,5 @@
         @endforelse
       </div><!-- Main row end -->
     </div><!-- Conatiner end -->
-
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
   </section><!-- Main container end -->
   @endsection
